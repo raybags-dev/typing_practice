6bea0183a8070771f29c37a7e62f98f08c6ea382
Raymond Baguma <you@example.com>
2020-11-11 09:14:15 +0100

    Implimenting local storage -track score

---
+++ public/js/component.js
@@ -0,0 +1,12 @@
+// ============= local storage Impimentation=================// 
+
+
+
+
+// ============= local storage Impimentation=================// 
+
+export {
+    x as comoponent_1,
+    y as comoponent_2
+
+};
