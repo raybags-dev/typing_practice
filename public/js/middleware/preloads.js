85cceada72c91ed85ea858d191f6ebfe054e943f
Raymond Baguma <you@example.com>
2020-11-16 10:24:37 +0100

    statics implimentation

--- public/js/middleware/preloads.js
+++ public/js/middleware/preloads.js
@@ -1,8 +1,12 @@
+'use strict'
 import { heading, spinnerIcon, autoGuidePopUp, guideParagraphs, heading_icon, testWrapper, originText, text_main_container, } from "../anchors.js";
 
+// fade in header  after page loads
 function preloads() {
-    // fade in header  after page loads
     $(window).on('load', function() {
+        let error = window.onerror;
+        if (error !== null) return console.error('Resources could not be loaded successfully');
+
         heading.classList.remove('hide')
         heading_icon.classList.remove('hide');
         heading.classList.add('fadeIn');
@@ -17,14 +21,15 @@ function preloads() {
         testWrapper.style.cssText = 'border: .4rem solid #967070;';
         // automatice guide popup
         showGuide()
-            //====Spinner funtionality======//
+            //Spinner funtionality//
         $(spinnerIcon).fadeOut("slow");;
+        console.log('All resources loaded successfully');
+
     });
 };
 
 function showGuide() {
     setTimeout(() => {
-        console.log('loaded successfully');
         autoGuidePopUp.click();
     }, 5000)
     guideParagraphs.forEach(element => {
@@ -32,5 +37,4 @@ function showGuide() {
         element.style.cssText = "border-left: .4rem solid rgb(150, 112, 112); border-right: .4rem solid rgb(150, 112, 112); box-shadow: 2px 2px 9px rgb(0,0,0,.6); border-radius: .3rem; transition: .2s;"
     });
 }
-
 export { preloads };
